5580d509cd62c0074f47bc00541a290c614ef57b
Pranshu Pandey <pranshu0604@gmail.com>
2025-03-18 14:46:33 +0530

    secure commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -14,20 +14,22 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="bg-[#F4C430] text-[#111827] flex flex-col items-center py-20 relative">
-        <div className="max-w-4xl text-left">
-          <p className="text-lg mb-8">
-            At Quantalyze, we are storytellers, strategists, and digital innovators passionate about helping brands thrive in the ever-evolving world of social media.
-          </p>
-          <p className="text-lg">
-            Our mission is to connect brands with their audiences in meaningful ways, creating authentic and impactful digital experiences that drive engagement, loyalty, and growth.
-          </p>
+      <section className="bg-[#F4C430] text-[#111827] flex flex-col items-center py-48 relative">
+        <div className="grid grid-cols-5 w-full px-20 min-h-[400px]">
+          <div className="col-span-4">
+            <p className="text-xl mb-8">
+              At Quantalyze, we are storytellers, strategists, and digital innovators passionate about helping brands thrive in the ever-evolving world of social media.
+            </p>
+            <p className="text-xl">
+              Our mission is to connect brands with their audiences in meaningful ways, creating authentic and impactful digital experiences that drive engagement, loyalty, and growth.
+            </p>
+          </div>
+          <div className="col-span-1 flex justify-center items-center">
+            <h3 className="text-9xl font-bold text-[#111827] transform rotate-90 origin-center whitespace-pre">WHO WE{'\n'}ARE</h3>
+          </div>
         </div>
-        <div className="mt-20">
+        <div className="mt-40 w-full">
           <Image src="/ads.jpg" alt="Ads" width={1920} height={1080} className="w-full h-auto object-contain" />
-        </div>
-        <div className="absolute right-0 top-1/2 transform -translate-y-1/2 -translate-x-20">
-          <h3 className="text-9xl font-bold text-[#111827] transform rotate-90 origin-center whitespace-pre">WHO WE{'\n'}ARE</h3>
         </div>
       </section>
     </>
